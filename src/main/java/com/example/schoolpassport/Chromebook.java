a9577d3cc71753141439f7cc7c2b60b14e39fa52
tayesh25 <tayesh25@sciencleadership.org>
2024-02-22 09:08:18 -0500

    Chromebook Class

---
+++ src/main/java/com/example/schoolpassport/Chromebook.java
@@ -0,0 +1,43 @@
+package com.example.schoolpassport;
+
+import java.time.LocalDate;
+
+public class Chromebook extends IdentificationCard {
+
+    private int cbid;
+
+    public int getCbid() {
+        return cbid;
+    }
+
+    public void setCbid(int cbid) {
+        this.cbid = cbid;
+    }
+
+    public boolean isHaveornothave() {
+        return haveornothave;
+    }
+
+    public void setHaveornothave(boolean haveornothave) {
+        this.haveornothave = haveornothave;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private boolean haveornothave;
+
+    private String email;
+
+
+    public Chromebook(String name, int cbid, int id, LocalDate mydob, String graduationyr, boolean haveornothave,String email) {
+        super(name, id, mydob, graduationyr);
+
+
+    }
+}
