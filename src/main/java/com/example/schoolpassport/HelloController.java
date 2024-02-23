2d94ce3e5aa55544a7437d61e0d890ff1fb0edd2
kingfisheii <132377565+kingfisheii@users.noreply.github.com>
2024-02-23 08:19:45 -0500

    asdf

--- src/main/java/com/example/schoolpassport/HelloController.java
+++ src/main/java/com/example/schoolpassport/HelloController.java
@@ -24,23 +24,16 @@ public class HelloController {
     public TextField AudioID;
     public ListView Chromebook;
     @FXML
-    protected void onHelloButtonClick() throws IOException {
-        String name = "Eli";
+    protected void adminMode() throws IOException {}
+    protected void addData() throws IOException {}
+    protected void ReadData() throws IOException {}
+    protected void Save() throws IOException {}
+    protected void Restore() throws IOException {}
+    protected void addminRemove() throws IOException {}
+    protected void TakeFaceCameShot() throws IOException {}
+    protected void pullFromItem() throws IOException {}
 
-        String dateString = DateOfBirth.getText();  // Get the date of birth from the TextField
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy,MM,dd");
-        LocalDate myDate = LocalDate.parse(dateString, formatter);
 
-        String email = "kingfisheiei@gmail.com";
-        Double studentID = 12.0;
-        Double gradYear = 2.0;
-        Boolean chromebook = false;
-        int numberOfChromebook = 2;
-
-        GoIn goInInstance = new GoIn(name, myDate, email, studentID, gradYear, chromebook, numberOfChromebook);
-        System.out.println(goInInstance.toString());
-
-    }
 }
 
